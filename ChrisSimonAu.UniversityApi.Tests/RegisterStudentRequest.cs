ce2703cb22e3b32e98f7f4e9da508ad03a36f99a
Chris Simon <chris@devcycles.io>
2023-06-04 13:23:38 +0000

    3-RED: Update test to assert on location header.

--- ChrisSimonAu.UniversityApi.Tests/RegisterStudentRequest.cs
+++ ChrisSimonAu.UniversityApi.Tests/RegisterStudentRequest.cs
@@ -1,9 +1,6 @@
 namespace ChrisSimonAu.UniversityApi.Tests;
 
-using System;
-
-public class StudentResponse
+public class RegisterStudentRequest
 {
-    public Guid Id {get; set;}
     public string? Name {get; set;}
 }
