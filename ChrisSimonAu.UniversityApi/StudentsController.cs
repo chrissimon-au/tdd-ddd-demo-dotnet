4a40811ee1064d7fcf5fa9dc818f333ffd53d94f
Chris Simon <chris@devcycles.io>
2023-06-04 13:01:56 +0000

    1-GREEN: Implement URL that returns 201 Created.

---
+++ ChrisSimonAu.UniversityApi/StudentsController.cs
@@ -0,0 +1,14 @@
+namespace ChrisSimonAu.UniversityApi;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class StudentsController : ControllerBase
+{
+    [HttpPost]
+    public ActionResult Register()
+    {
+        return Created("", null);
+    }
+}
