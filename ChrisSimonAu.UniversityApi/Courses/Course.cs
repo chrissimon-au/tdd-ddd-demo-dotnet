c8a5a5975626026025cd79a0a8a570960ed65471
Chris Simon <chris@devcycles.io>
2023-06-05 21:40:34 +0000

    18-REFACTOR: Move course construction into a static factory method.

--- ChrisSimonAu.UniversityApi/Courses/Course.cs
+++ ChrisSimonAu.UniversityApi/Courses/Course.cs
@@ -3,4 +3,8 @@ namespace ChrisSimonAu.UniversityApi.Courses;
 public class Course
 {
     public Guid Id { get; set; }
+    public static Course IncludeInCatalog()
+    {
+        return new Course { Id = Guid.NewGuid() };
+    }
 }
