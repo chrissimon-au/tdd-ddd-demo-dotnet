9e617ac4d4bf4ca837544aa294f5c01234bdcf93
Chris Simon <chris@devcycles.io>
2023-06-05 21:47:39 +0000

    18-REFACTOR: Refactor courseApi out of tests.

---
+++ ChrisSimonAu.UniversityApi.Tests/Courses/CourseApi.cs
@@ -0,0 +1,18 @@
+namespace ChrisSimonAu.UniversityApi.Tests.Courses;
+
+public class CourseApi
+{
+    private readonly HttpClient client;
+
+    public CourseApi(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task<(HttpResponseMessage, CourseResponse?)> IncludeInCatalog()
+    {
+        var response = await client.PostAsync("/courses", null);
+        var course = await response.Content.ReadFromJsonAsync<CourseResponse>();
+        return (response, course);
+    }
+}
