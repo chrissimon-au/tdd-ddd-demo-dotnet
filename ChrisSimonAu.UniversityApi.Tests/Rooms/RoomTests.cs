ccb2e33e35142d03e3ce1889e842dc5657d9d0b9
Chris Simon <chris@devcycles.io>
2023-06-05 06:21:07 +0000

    11-RED: Add test to assert on location header.

--- ChrisSimonAu.UniversityApi.Tests/Rooms/RoomTests.cs
+++ ChrisSimonAu.UniversityApi.Tests/Rooms/RoomTests.cs
@@ -20,6 +20,7 @@ public class RoomTests : IClassFixture<WebApplicationFactory<Program>>
 
         ItShouldSetupANewRoom(response);
         ItShouldAllocateANewId(room);
+        ItShouldShowWhereToLocateNewRoom(response, api.UriForRoomId(room?.Id));
     }
 
     private void ItShouldSetupANewRoom(HttpResponseMessage response)
@@ -31,5 +32,12 @@ public class RoomTests : IClassFixture<WebApplicationFactory<Program>>
     {
         Assert.NotNull(room);
         Assert.NotEqual(room.Id, Guid.Empty);
+    }
+
+    private void ItShouldShowWhereToLocateNewRoom(HttpResponseMessage response, Uri roomUri)
+    {
+        var location = response.Headers.Location;
+        Assert.NotNull(location);
+        Assert.Equal(roomUri, location);
     }
 }
