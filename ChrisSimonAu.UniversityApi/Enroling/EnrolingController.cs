164c0376760a28de60325960a5c6f0fe2fd3aa49
Chris Simon <chris@devcycles.io>
2023-06-06 14:11:25 +0000

    30-GREEN: Add implementation that checks for student Id before attempting to enrol.

--- ChrisSimonAu.UniversityApi/Enroling/EnrolingController.cs
+++ ChrisSimonAu.UniversityApi/Enroling/EnrolingController.cs
@@ -6,10 +6,20 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class EnrolingController : ControllerBase
 {
+    private readonly UniversityContext context;
+
+    public EnrolingController(UniversityContext context)
+    {
+        this.context = context;
+    }
+
     [HttpPost]
     [Route("/students/{studentId}/courses")]
-    public IActionResult EnrolInCourse([FromRoute] Guid studentId, [FromBody] EnrolStudentInCourseRequest request)
+    public async Task<ActionResult<Enrolment>> EnrolInCourse([FromRoute] Guid studentId, [FromBody] EnrolStudentInCourseRequest request)
     {
-        return Created("", new Enrolment { Id = Guid.NewGuid(), StudentId = studentId, CourseId = request.CourseId });
+        var student = await context.Students.FindAsync(studentId);
+        return student == null ?
+            NotFound() :
+            Created("", new Enrolment { Id = Guid.NewGuid(), StudentId = studentId, CourseId = request.CourseId });
     }
 }
