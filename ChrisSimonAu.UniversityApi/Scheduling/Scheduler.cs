4587a3b0280e2f764d428c729beb4d6795094b2a
Chris Simon <chris@devcycles.io>
2023-06-06 22:29:29 +0000

    39-GREEN: Simple implementation of scheduler that only works for a single course and single room.

--- ChrisSimonAu.UniversityApi/Scheduling/Scheduler.cs
+++ ChrisSimonAu.UniversityApi/Scheduling/Scheduler.cs
@@ -1,11 +1,23 @@
 namespace ChrisSimonAu.UniversityApi.Scheduling;
 
 using ChrisSimonAu.UniversityApi.Rooms;
+using Courses;
 
 public class Scheduler
 {
     public static Schedule ScheduleCourses(List<CourseEnrolment> courseEnrolments, List<Room> rooms)
     {
-        throw new NotImplementedException();
+        var schedule = new Schedule()
+        {
+            Courses = new List<Course>()
+        };
+        
+        foreach (var courseEnrolmentRoom in courseEnrolments.Zip(rooms))
+        {
+            var course = courseEnrolmentRoom.First.Course;
+            course.Room = courseEnrolmentRoom.Second;
+            schedule.Courses.Add(course);
+        }
+        return schedule;
     }
 }
