751e6d2f901cb26eaf2d919a8eb9aef35eb8d7d5
Chris Simon <chris@devcycles.io>
2023-06-06 03:54:30 +0000

    20-GREEN: add simple implementation that returns course details on including a new course.

--- ChrisSimonAu.UniversityApi/Courses/Course.cs
+++ ChrisSimonAu.UniversityApi/Courses/Course.cs
@@ -3,8 +3,9 @@ namespace ChrisSimonAu.UniversityApi.Courses;
 public class Course
 {
     public Guid Id { get; set; }
-    public static Course IncludeInCatalog()
+    public string? Name { get; set; }
+    public static Course IncludeInCatalog(IncludeCourseInCatalogRequest request)
     {
-        return new Course { Id = Guid.NewGuid() };
+        return new Course { Id = Guid.NewGuid(), Name = request.Name };
     }
 }
