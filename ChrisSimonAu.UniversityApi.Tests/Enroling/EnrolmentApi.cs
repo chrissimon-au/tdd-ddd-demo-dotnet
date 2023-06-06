6b3ffa84ade3e4a30d3fbe7e2e9daccd9cf345a5
Chris Simon <chris@devcycles.io>
2023-06-06 13:53:29 +0000

    27-RED: Add test to confirm new enrolment on successful enrolment.

--- ChrisSimonAu.UniversityApi.Tests/Enroling/EnrolmentApi.cs
+++ ChrisSimonAu.UniversityApi.Tests/Enroling/EnrolmentApi.cs
@@ -12,9 +12,11 @@ public class EnrolmentApi
         this.client = client;
     }
 
-    public async Task<HttpResponseMessage> EnrolStudentInCourse(StudentResponse? student, CourseResponse? course)
+    public async Task<(HttpResponseMessage, EnrolmentResponse?)> EnrolStudentInCourse(StudentResponse? student, CourseResponse? course)
     {
         var request = new EnrolStudentInCourseRequest { CourseId = course?.Id };
-        return await client.PostAsync($"/students/{student?.Id}/courses", JsonContent.Create(request));
+        var response = await client.PostAsync($"/students/{student?.Id}/courses", JsonContent.Create(request));
+        var enrolment = await response.Content.ReadFromJsonAsync<EnrolmentResponse>();
+        return (response, enrolment);
     }
 }
