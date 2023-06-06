9d1f0901f20d62aa15970a4e5ceeb85df0dac413
Chris Simon <chris@devcycles.io>
2023-06-06 04:02:02 +0000

    21-GREEN: add implementation using dbcontext to save and retrieve courses.

--- ChrisSimonAu.UniversityApi/Courses/CourseController.cs
+++ ChrisSimonAu.UniversityApi/Courses/CourseController.cs
@@ -6,10 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 [Route("[controller]")]
 public class CoursesController : ControllerBase
 {
+    private readonly UniversityContext context;
+
+    public CoursesController(UniversityContext context)
+    {
+        this.context = context;
+    }
+
     [HttpPost]
-    public ActionResult<Course> IncludeInCatalog([FromBody] IncludeCourseInCatalogRequest request)
+    public async Task<ActionResult<Course>> IncludeInCatalog([FromBody] IncludeCourseInCatalogRequest request)
     {
         var course = Course.IncludeInCatalog(request);
+        await context.Courses.AddAsync(course);
+        await context.SaveChangesAsync();
         return Created($"http://localhost/courses/{course.Id}", course);
+    }
+
+    [HttpGet]
+    [Route("/courses/{id}")]
+    public async Task<ActionResult<Course>> Get([FromRoute] Guid id)
+    {
+        var course = await context.Courses.FindAsync(id);
+        return Ok(course);
     }
 }
