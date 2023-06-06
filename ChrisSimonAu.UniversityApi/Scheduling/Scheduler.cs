bc9090d8e091d05afc321aec60b05934bee503bf
Chris Simon <chris@devcycles.io>
2023-06-06 22:22:57 +0000

    39-RED: Unit test around independent scheduler domain service.

---
+++ ChrisSimonAu.UniversityApi/Scheduling/Scheduler.cs
@@ -0,0 +1,11 @@
+namespace ChrisSimonAu.UniversityApi.Scheduling;
+
+using ChrisSimonAu.UniversityApi.Rooms;
+
+public class Scheduler
+{
+    public static Schedule ScheduleCourses(List<CourseEnrolment> courseEnrolments, List<Room> rooms)
+    {
+        throw new NotImplementedException();
+    }
+}
