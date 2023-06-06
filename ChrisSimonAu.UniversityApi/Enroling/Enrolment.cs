06641b573d6ac017cd5bf824d2d4beb31cfb7a0e
Chris Simon <chris@devcycles.io>
2023-06-06 20:25:17 +0000

    34-REFACTOR: move to using lazy loaded reference properties for enrolments and courses.

--- ChrisSimonAu.UniversityApi/Enroling/Enrolment.cs
+++ ChrisSimonAu.UniversityApi/Enroling/Enrolment.cs
@@ -1,8 +1,21 @@
 namespace ChrisSimonAu.UniversityApi.Enroling;
 
+using Students;
+using Courses;
+
 public class Enrolment
 {
     public Guid? Id { get; set; }
-    public Guid? StudentId { get; set; }
-    public Guid? CourseId { get; set; }
+    public virtual Student? Student { get; set; }
+    public virtual Course? Course { get; set; }
+
+    public static Enrolment StudentEnroled(Student student, Course course)
+    {
+        return new Enrolment { Id = Guid.NewGuid(), Student = student, Course = course };
+    }
+
+    public EnrolmentResponse ToResponse()
+    {
+        return new EnrolmentResponse { Id = Id, StudentId = Student?.Id, CourseId = Course?.Id };
+    }
 }
---
+++ ChrisSimonAu.UniversityApi/Enroling/EnrolmentResponse.cs
@@ -0,0 +1,8 @@
+namespace ChrisSimonAu.UniversityApi.Enroling;
+
+public class EnrolmentResponse
+{
+    public Guid? Id { get; set; }
+    public Guid? StudentId { get; set; }
+    public Guid? CourseId { get; set; }
+}
