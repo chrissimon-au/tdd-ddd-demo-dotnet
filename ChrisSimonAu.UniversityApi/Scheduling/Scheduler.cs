5ab07fbcbdd19d84d613178eff222c4dd3f7ecdb
Chris Simon <chris@devcycles.io>
2023-06-07 03:56:40 +0000

    40-REFACTOR: extract some methods out of the scheduling algorithm to use intention revealing names.

--- ChrisSimonAu.UniversityApi/Scheduling/Scheduler.cs
+++ ChrisSimonAu.UniversityApi/Scheduling/Scheduler.cs
@@ -5,20 +5,20 @@ using Courses;
 
 public class Scheduler
 {
+    private static Comparison<CourseEnrolment> ByMostPopular = (ce1, ce2) => ce2.EnrolmentCount.CompareTo(ce1.EnrolmentCount);
+    private static Comparison<Room> ByMostSpacious =  (r1, r2) => r2.Capacity.CompareTo(r1.Capacity);
+
     public static Schedule ScheduleCourses(List<CourseEnrolment> courseEnrolments, List<Room> rooms)
     {
-        var schedule = new Schedule()
-        {
-            Courses = new List<Course>()
-        };
+        var schedule = new Schedule();
 
-        courseEnrolments.Sort((ce1, ce2) => ce2.EnrolmentCount.CompareTo(ce1.EnrolmentCount));
-        rooms.Sort((r1, r2) => r2.Capacity.CompareTo(r1.Capacity));
+        courseEnrolments.Sort(ByMostPopular);
+        rooms.Sort(ByMostSpacious);
         
-        foreach (var courseEnrolmentRoom in courseEnrolments.Zip(rooms))
+        foreach (var (courseEnrolment, room) in courseEnrolments.Zip(rooms))
         {
-            var course = courseEnrolmentRoom.First.Course;
-            course.Room = courseEnrolmentRoom.Second;
+            var course = courseEnrolment.Course;
+            course.AssignTo(room);
             schedule.Courses.Add(course);
         }
         return schedule;
