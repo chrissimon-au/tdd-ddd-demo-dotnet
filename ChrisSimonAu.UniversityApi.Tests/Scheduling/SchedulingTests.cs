f491ca45f961c6d3378f19d9f6c0b1c07dd672ee
Chris Simon <chris@devcycles.io>
2023-06-07 05:25:24 +0000

    42-RED: Add test for more complex scheduling results from API

--- ChrisSimonAu.UniversityApi.Tests/Scheduling/SchedulingTests.cs
+++ ChrisSimonAu.UniversityApi.Tests/Scheduling/SchedulingTests.cs
@@ -8,17 +8,11 @@ using Students;
 
 public class SchedulingTests : IClassFixture<WebApplicationFactory<Program>>
 {
-    private readonly WebApplicationFactory<Program> _factory;
-
-    public SchedulingTests(WebApplicationFactory<Program> factory)
-    {
-        _factory = factory;
-    }
 
     [Fact]
-    public async Task GivenIAmAnAdmin_WhenIScheduleCourses()
+    public async Task GivenThereIsASingleCourseAndRoom_WhenIScheduleCourses()
     {
-        var client = _factory.CreateClient();
+        var client = new WebApplicationFactory<Program>().CreateClient();
         var schedulingApi = new SchedulingApi(client);
         var studentApi = new StudentApi(client);
         var courseApi = new CourseApi(client);
@@ -53,6 +47,50 @@ public class SchedulingTests : IClassFixture<WebApplicationFactory<Program>>
         Assert.Equal(course?.Id, scheduledCourse.Id);
         Assert.Equal(course?.Name, scheduledCourse.Name);
         Assert.Equal(room?.Id, scheduledCourse?.RoomId);
+    }
+
+    [Fact]
+    public async Task GivenThereAreMultipleCoursesAndRooms_WhenIScheduleCourses()
+    {
+        var client = new WebApplicationFactory<Program>().CreateClient();
+        var schedulingApi = new SchedulingApi(client);
+        var studentApi = new StudentApi(client);
+        var courseApi = new CourseApi(client);
+        var roomApi = new RoomApi(client);
+        var enrolmentApi = new EnrolmentApi(client);
+
+        var (_, course1) = await courseApi.IncludeInCatalog(new IncludeCourseInCatalogRequest { Name = "First Course" });
+        var (_, course2) = await courseApi.IncludeInCatalog(new IncludeCourseInCatalogRequest { Name = "Second Course" });
+
+        var (_, student1) = await studentApi.RegisterStudent(new RegisterStudentRequest { Name = "Student 1" });
+        var (_, student2) = await studentApi.RegisterStudent(new RegisterStudentRequest { Name = "Student 2" });
+        var (_, student3) = await studentApi.RegisterStudent(new RegisterStudentRequest { Name = "Student 3" });
+
+        await enrolmentApi.EnrolStudentInCourse(student1, course1);
+        await enrolmentApi.EnrolStudentInCourse(student2, course1);
+
+        await enrolmentApi.EnrolStudentInCourse(student1, course2);
+        await enrolmentApi.EnrolStudentInCourse(student2, course2);
+        await enrolmentApi.EnrolStudentInCourse(student3, course2);
+
+        var (_, room1) = await roomApi.SetupRoom(new SetupRoomRequest { Name = "Room1", Capacity = 5 });
+        var (_, room2) = await roomApi.SetupRoom(new SetupRoomRequest { Name = "Room2", Capacity = 2 });
+
+        var (response, schedule) = await schedulingApi.Schedule();
+
+        ItShouldScheduleCourseToRoom(schedule, course1!, room2!);
+        ItShouldScheduleCourseToRoom(schedule, course2!, room1!);
+    }
+
+    private void ItShouldScheduleCourseToRoom(ScheduleResponse? schedule, CourseResponse course, RoomResponse room)
+    {
+        var scheduledCourses = schedule?.ScheduledCourses;
+        Assert.NotNull(scheduledCourses);
+        
+        var scheduledCourse = scheduledCourses.FirstOrDefault(sc => sc.Id == course?.Id);
+
+        Assert.Equal(course.Name, scheduledCourse?.Name);
+        Assert.Equal(room.Id, scheduledCourse?.RoomId);
     }
 
 }
